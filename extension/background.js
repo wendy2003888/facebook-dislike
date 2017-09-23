d4ec2c09ceb693bbdcf435069b4ead763f07ac81
stormouse <stormouse@126.com>
2017-09-23 16:30:54 -0700

    a little bit organizing work

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,55 @@
 
 chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse){
-        // alert("message received");
-        alert("message <" + message + "> received");
-        
-        // send message to remote API
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function(){
-            alert(xhr.status);
-            if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
-                alert(xhr.responseText);
-            }
+    function(msg){
+        if(msg.action == "dummy_test"){
+            testRemoteCall();
         }
-
-
-
-        xhr.open("GET", chrome.extension.getURL('http://127.0.0.1:5000/student/2'), true);
-        xhr.send();
-
+        else if(msg.action == "user_dislike"){
+            userDislike(msg.user_id, msg.post_id);
+        }
+        else if(msg.action == "user_undislike"){
+            userUndislike(msg.user_id, msg.post_id);
+        }
+        else if(msg.action == "get_single_dislike_count"){
+            getPostDislikeCount(msg.post_id);
+        }
+        else if(msg.action == "get_multiple_dislike_count"){
+            getPostsDislikeCount(msg.post_id_list);
+        }
     }
 );
 
 
+function testRemoteCall(){
+    // send message to remote API
+    var xhr = new XMLHttpRequest();
+        
+    xhr.open("GET", 'http://echo.jsontest.com/title/ipsum/content/blah', true);
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+            alert(xhr.responseText);
+        }
+    }
+
+    xhr.send();
+}
+
+
+function userDislike(user_id, post_id){
+
+}
+
+function userUndislike(user_id, post_id){
+
+}
+
+function getPostDislikeCount(post_id){
+
+}
+
+function getPostsDislikeCount(post_id_list){
+
+}
 
 
--- extension/test.js
+++ extension/test.js
@@ -4,9 +4,10 @@ var button = document.createElement("button");
 button.innerHTML = "Do something";
 
 button.addEventListener("click", function(){
-    chrome.runtime.sendMessage("test");
+    chrome.runtime.sendMessage({action:"dummy_test"});
 });
 
 var body = document.getElementsByTagName("body")[0];
+
 body.appendChild(button);
 
