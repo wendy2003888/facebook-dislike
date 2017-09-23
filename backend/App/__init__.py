5d2dd29cb087affa99b787e4899ce21085d82f66
wendy2003888 <wendyh2003888@gmail.com>
2017-09-23 15:30:25 -0700

    enable cros

--- backend/App/__init__.py
+++ backend/App/__init__.py
@@ -2,9 +2,12 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+CORS(app)
 
