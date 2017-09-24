bc2f520aced7392ef2b04750a3e1d5dc9610c310
stormouse <stormouse@126.com>
2017-09-24 06:47:00 -0700

    basic function complete

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@
 chrome.runtime.onMessage.addListener(
     function(msg){
         if(msg.action == "dummy_test"){
-            testRemoteCall();
+            testCreateOverlay();
         }
         else if(msg.action == "user_dislike"){
             userDislike(msg.user_id, msg.post_id);
@@ -10,11 +10,8 @@ chrome.runtime.onMessage.addListener(
         else if(msg.action == "user_undislike"){
             userUndislike(msg.user_id, msg.post_id);
         }
-        else if(msg.action == "get_single_dislike_count"){
-            getPostDislikeCount(msg.post_id);
-        }
-        else if(msg.action == "get_multiple_dislike_count"){
-            getPostsDislikeCount(msg.post_id_list);
+        else if(msg.action == "get_multiple_dislike_info"){
+            getPostsDislikeInfo(msg.user_id, msg.post_id_list);
         }
     }
 );
@@ -36,20 +33,90 @@ function testRemoteCall(){
 }
 
 
-function userDislike(user_id, post_id){
+function testCreateOverlay(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs.id, {action:"createCanvas", width:"500px", height:"500px", innerHTML:"Hello"}, function(response){
+            console.log(response.status);
+        });
+    });
+}
 
+
+function userDislike(user_id, post_id){
+    // send message to remote API
+
+    var data = JSON.stringify(
+        {
+            'user_id': user_id,
+            'post_id': post_id
+        }
+    );
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://107.170.237.213:8000/user_dislike', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+            
+        }
+    }
+
+    xhr.send(data);
 }
 
 function userUndislike(user_id, post_id){
 
+    var data = JSON.stringify(
+        {
+            'user_id': user_id,
+            'post_id': post_id
+        }
+    );
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://107.170.237.213:8000/user_undislike', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+
+        }
+    }
+
+    xhr.send(data);
 }
 
-function getPostDislikeCount(post_id){
 
+
+function getPostsDislikeInfo(user_id, post_id_list){
+    var data = JSON.stringify(
+        {
+            'user_id': user_id,
+            'posts': post_id_list
+        }
+    );
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://107.170.237.213:8000/get_dislike_info', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+            var jdata = JSON.parse(xhr.responseText);
+            var obj = {
+                disliked: jdata.user_dislikes, 
+                dislike_count: jdata.dislike_count
+            };
+            callRefreshContent(obj);
+        }
+    }
+
+    xhr.send(data);
 }
 
-function getPostsDislikeCount(post_id_list){
 
+
+function callRefreshContent(data){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action:"render", data:{disliked:data.disliked, dislike_count:data.dislike_count}}, function(response) {});
+    });
 }
-
-
--- extension/test.js
+++ extension/test.js
@@ -1,13 +1,134 @@
 
 
+function CreateDiv(width_s, height_s, inner_html){
+    console.log("Created");
+    var div = document.createElement("div");
+    div.style.width = width_s;
+    div.style.height = height_s;
+    div.innerHTML = inner_html;
+    div.setAttribute("style", "position:absolute;top:0px;left:0px;z-index:255;");
+    document.body.appendChild(div);
+}
+
+
+var canvas = null;
+var ctx = null;
+var flag = false;
+var cv_width, cv_height;
+var prevX = 0, currX = 0, prevY = 0, currY = 0;
+var dot_flag = 0;
+
+
+function CreateCanvas(width_s, height_s){
+    canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = "absolute";
+    canvas.style.top = "0px";
+    canvas.style.left = "0px";
+    canvas.style.zIndex = "512";
+    document.body.appendChild(canvas);
+
+    initCanvas();
+}
+
+function initCanvas(){
+    // set global variables
+    ctx = canvas.getContext("2d");
+    cv_width = canvas.width;
+    cv_height = canvas.height;
+
+    canvas.addEventListener("mousemove", function(e){
+        findxy('move', e);
+    });
+    canvas.addEventListener("mousedown", function(e){
+        findxy('down', e);
+    });
+    canvas.addEventListener("mouseup", function(e){
+        findxy('up', e);
+    });
+    canvas.addEventListener("mouseout", function(e){
+        findxy('out', e);
+    });
+}
+
+
+function draw(){
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = "2";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = "black";
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+    }
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if(request.action == "createCanvas"){
+            CreateCanvas(request.width, request.height);
+            sendResponse({status: "success"});
+        }
+    }
+);
+
+
+
 var button = document.createElement("button");
+button.setAttribute("style", "z-index:255;");
 button.innerHTML = "Do something";
+
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+
 
 button.addEventListener("click", function(){
     chrome.runtime.sendMessage({action:"dummy_test"});
+    // chrome.runtime.sendMessage({action:"user_dislike", user_id:'1234567812345678', post_id:'1234567812345678_0123456'});
 });
 
-var body = document.getElementsByTagName("body")[0];
 
-body.appendChild(button);
+var btn_toggle_canvas = document.createElement("button");
+btn_toggle_canvas.innerHTML = "Toggle Canvas Hittest";
+btn_toggle_canvas.addEventListener("click", function(){
+    if(canvas.style.pointerEvents == "none")
+        canvas.style.pointerEvents = "auto";
+    else
+        canvas.style.pointerEvents = "none";
+});
 
+body.appendChild(btn_toggle_canvas);
